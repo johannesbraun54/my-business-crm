7eec9c4f0204bbab93037154fbdf1110b65cf257
johannesbraun54 <johannesbraun02@gmail.com>
2024-03-06 15:56:44 +0100

    responsivness step two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, ViewChild, inject } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -15,14 +15,14 @@ import { AuthService } from './service/authService.service';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,
-            RouterOutlet,
-            MatSidenavModule,
-            MatToolbarModule,
-            MatIconModule,
-            RouterModule,
-            MatDatepickerModule,
-            MatNativeDateModule]
-            ,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule,
+    MatDatepickerModule,
+    MatNativeDateModule]
+  ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [MatDatepickerModule],
@@ -30,8 +30,25 @@ import { AuthService } from './service/authService.service';
 export class AppComponent {
   title = 'simple-crm';
   firestore: Firestore = inject(Firestore);
+  @ViewChild('drawer') drawer!: MatDrawer;
 
-  constructor(public authService: AuthService){
+  constructor(public authService: AuthService) {
 
   }
+
+  isDrawerOpened(): boolean {
+    return this.drawer.opened ? true : false
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (window.innerWidth <= 1000 && this.isDrawerOpened()) {
+      this.drawer.close();
+    }
+
+    if(window.innerWidth > 1000 && !this.isDrawerOpened()) {
+      this.drawer.open();
+    }
+  }
+
 }
